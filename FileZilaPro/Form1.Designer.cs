6c8c26b0dbc6466aec2004f70c333a32117d8f7a
AyseTeam <86293015+AyseTeam@users.noreply.github.com>
2021-06-22 14:06:42 +0200

    Add files via upload

---
+++ FileZilaPro/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace FileZilaPro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inst = new System.Windows.Forms.Button();
+            this.por = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.End2 = new System.Windows.Forms.Timer(this.components);
+            this.End1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox9.BackgroundImage")));
+            this.pictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox9.Location = new System.Drawing.Point(250, 462);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox9.TabIndex = 18;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox8.BackgroundImage")));
+            this.pictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox8.Location = new System.Drawing.Point(148, 462);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox8.TabIndex = 17;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImage = global::FileZillaPro.Properties.Resources.icons8_facebook_24px;
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox7.Location = new System.Drawing.Point(44, 462);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(55, 51);
+            this.pictureBox7.TabIndex = 16;
+            this.pictureBox7.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10.25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(150, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "contact";
+            // 
+            // inst
+            // 
+            this.inst.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.inst.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.inst.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inst.ForeColor = System.Drawing.Color.White;
+            this.inst.Location = new System.Drawing.Point(63, 293);
+            this.inst.Name = "inst";
+            this.inst.Size = new System.Drawing.Size(227, 66);
+            this.inst.TabIndex = 14;
+            this.inst.Text = "Installer";
+            this.inst.UseVisualStyleBackColor = true;
+            this.inst.Click += new System.EventHandler(this.inst_Click);
+            // 
+            // por
+            // 
+            this.por.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(184)))), ((int)(((byte)(206)))));
+            this.por.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.por.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(38)))), ((int)(((byte)(48)))));
+            this.por.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.por.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(38)))), ((int)(((byte)(48)))));
+            this.por.Location = new System.Drawing.Point(63, 211);
+            this.por.Name = "por";
+            this.por.Size = new System.Drawing.Size(227, 66);
+            this.por.TabIndex = 13;
+            this.por.Text = "Portable";
+            this.por.UseVisualStyleBackColor = false;
+            this.por.Click += new System.EventHandler(this.por_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox4.Location = new System.Drawing.Point(125, 20);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 89);
+            this.pictureBox4.TabIndex = 19;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(129, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 19);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "FileZilla Pro";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.progressBar1.Location = new System.Drawing.Point(63, 387);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(227, 23);
+            this.progressBar1.TabIndex = 21;
+            // 
+            // End2
+            // 
+            this.End2.Tick += new System.EventHandler(this.End_Tick);
+            // 
+            // End1
+            // 
+            this.End1.Tick += new System.EventHandler(this.End1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(38)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(348, 529);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inst);
+            this.Controls.Add(this.por);
+            this.Font = new System.Drawing.Font("Arial", 12.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button inst;
+        private System.Windows.Forms.Button por;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer End2;
+        private System.Windows.Forms.Timer End1;
+    }
+}
+
---
+++ FileZilaPro/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+
+namespace FileZilaPro
+{
+    public partial class Form1 : Form
+    {
+        int _tick = 5;
+        int _tick2 = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void por_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                bool exist1 = File.Exists(@"C:\Users\" + Environment.UserName + @"\Desktop\FileZilla.Pro.3.52.2.Portable.zip");
+                bool exist11 = File.Exists(@"C:\Users\" + Environment.UserName + @"\Desktop\FileZilla.Pro.3.52.2.Portable");
+
+                if (exist1 || exist11)
+                {
+                    MessageBox.Show("File exist");
+                }
+                else
+                {
+                    progressBar1.Value = 10;
+
+                    progressBar1.Visible = true;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = 100;
+
+                    progressBar1.Value = 20;
+
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile("https://cdn.discordapp.com/attachments/824369227142135809/827214547021856788/FileZilla.Pro.3.52.2.Portable.zip", @"C:\Users\" + Environment.UserName + @"\Desktop\FileZilla.Pro.3.52.2.Portable.zip");
+
+                    progressBar1.Value = 70;
+
+                    string Name = "FileZilla.Pro.3.52.2.Portable.zip";
+                    string Path = @"C:\Users\" + Environment.UserName + @"\Desktop\";
+                    string zipPath = Path + Name;
+
+                    ZipFile.ExtractToDirectory(zipPath, @"C:\Users\" + Environment.UserName + @"\Desktop\");
+
+                    File.Delete(zipPath);
+
+                    progressBar1.Value = 78;
+
+
+                    bool exist3 = File.Exists(@"C:\Users\" + Environment.UserName + @"\Desktop\Readme-Portable.txt");
+
+                    if (exist3)
+                    {
+                        progressBar1.Value = 100;
+
+                        DialogResult res = System.Windows.Forms.MessageBox.Show("Do you want open the 'Readme-Portable.txt' ?          ", "Open", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                        if (res == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            Process.Start(@"C:\Users\" + Environment.UserName + @"\Desktop\Readme-Portable.txt");
+                            progressBar1.Visible = false;
+                            progressBar1.Value = 0;
+                            MessageBox.Show("It's installed ! 🐱‍🏍", "🐱‍🏍", MessageBoxButtons.OK, MessageBoxIcon.Information); ;
+                        }
+                        else
+                        {
+                            progressBar1.Visible = false;
+                            progressBar1.Value = 0;
+                            MessageBox.Show("It's installed ! 🐱‍🏍", "🐱‍🏍", MessageBoxButtons.OK, MessageBoxIcon.Information); ;
+                        }
+
+                    }
+                    else
+                    {
+                        WebClient Readme1 = new WebClient();
+                        Readme1.DownloadFile("https://cdn.discordapp.com/attachments/824369227142135809/827255469508788274/Readme-Portable.txt", @"C:\Users\" + Environment.UserName + @"\Desktop\Readme-Portable.txt");
+                        progressBar1.Value = 100;
+                        progressBar1.Value = 0;
+                        End1.Start();
+                        MessageBox.Show("It's installed ! 🐱‍🏍", "🐱‍🏍", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+
+
+            }
+            catch (WebException a)
+            {
+                progressBar1.Visible = false;
+                MessageBox.Show(a.ToString());
+            }
+        }
+
+        private void inst_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+
+                bool exist2 = File.Exists(@"C:\Users\"+Environment.UserName+ @"\Desktop\FileZilla.Pro.3.52.2_Installer.zip");
+                bool exist22 = File.Exists(@"C:\Users\" + Environment.UserName + @"\Desktop\FileZilla.Pro.3.52.2_Installer");
+
+                if (exist2 || exist22)
+                {
+                    MessageBox.Show("File exist");
+                }
+                else
+                {
+                    progressBar1.Value = 10;
+
+                    progressBar1.Visible = true;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = 100;
+
+                    progressBar1.Value = 20;
+
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile("https://cdn.discordapp.com/attachments/824369227142135809/827214044746088448/FileZilla.Pro.3.52.2_Installer.zip", @"C:\Users\" + Environment.UserName + @"\Desktop\FileZilla.Pro.3.52.2_Installer.zip");
+
+                    progressBar1.Value = 70;
+
+                    string Name = "FileZilla.Pro.3.52.2_Installer.zip";
+                        string Path = @"C:\Users\" + Environment.UserName + @"\Desktop\";
+                        string zipPath = Path + Name;
+
+                        ZipFile.ExtractToDirectory(zipPath, @"C:\Users\" + Environment.UserName + @"\Desktop\");
+
+                    File.Delete(zipPath);
+
+                    progressBar1.Value = 78;
+
+
+                    bool exist3 = File.Exists(@"C:\Users\" + Environment.UserName + @"\Desktop\Readme-Installer.txt");
+
+                    if (exist3)
+                    {
+                        progressBar1.Value = 100;
+
+                        DialogResult res = System.Windows.Forms.MessageBox.Show("Do you want open the 'Readme-Installer.txt' ?          ", "...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                        if (res == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            Process.Start(@"C:\Users\" + Environment.UserName + @"\Desktop\Readme-Installer.txt");
+                            progressBar1.Visible = false;
+                            progressBar1.Value = 0;
+                            MessageBox.Show("It's installed ! 🐱‍🏍", "🐱‍🏍", MessageBoxButtons.OK, MessageBoxIcon.Information); ;
+                        }
+                        else
+                        {
+                            progressBar1.Visible = false;
+                            progressBar1.Value = 0;
+                            MessageBox.Show("It's installed ! 🐱‍🏍", "🐱‍🏍", MessageBoxButtons.OK, MessageBoxIcon.Information); ;
+                        }
+
+                    }
+                    else
+                    {
+                        WebClient Readme2 = new WebClient();
+                        Readme2.DownloadFile("https://cdn.discordapp.com/attachments/824369227142135809/827231457298219008/Readme.txt", @"C:\Users\" + Environment.UserName + @"\Desktop\Readme-Installer.txt");
+                        progressBar1.Value = 100;
+                        progressBar1.Value = 0;
+                        End2.Start();
+                        MessageBox.Show("It's installed ! 🐱‍🏍", "🐱‍🏍", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+
+                
+            }
+            catch (WebException a)
+            {
+                progressBar1.Visible = false;
+                MessageBox.Show(a.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;
+        }
+
+        
+        private void End_Tick(object sender, EventArgs e)
+        {
+            _tick2--;
+
+            if (_tick2 == 0)
+            {
+                Process.Start(@"C:\Users\" + Environment.UserName + @"\Desktop\Readme-Installer.txt");
+                progressBar1.Visible = false;
+                progressBar1.Value = 0;
+                End2.Stop();
+            }
+        }
+
+        private void End1_Tick(object sender, EventArgs e)
+        {
+            _tick--;
+
+            if (_tick == 0)
+            {
+                Process.Start(@"C:\Users\" + Environment.UserName + @"\Desktop\Readme-Portable.txt");
+                progressBar1.Visible = false;
+                progressBar1.Value = 0;
+                End2.Stop();
+            }
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/VVh9WCw8Ym");
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (PreClosingConfirmation() == System.Windows.Forms.DialogResult.Yes)
+            {
+                Dispose(true);
+                Application.Exit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+
+            base.OnFormClosing(e);
+            if (progressBar1.Value > 1)
+            {
+                if (Installation() == System.Windows.Forms.DialogResult.Yes)
+                {
+                    Dispose(true);
+                    Application.Exit();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private DialogResult PreClosingConfirmation()
+        {
+            DialogResult res = System.Windows.Forms.MessageBox.Show("Do you want to leave `FileZilla Pro` ?          ", "Quit...", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            return res;
+        }
+
+        private DialogResult Installation()
+        {
+            DialogResult res = System.Windows.Forms.MessageBox.Show("FileZilla Pro is in installation, Do you went leave ?          ", "Quit...", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            return res;
+        }
+    }
+}
